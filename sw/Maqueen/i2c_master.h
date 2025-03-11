60c5909eccd9874a69b34be54a7ce0e71185b43d
Adrià Brú <adriabrucortes@gmail.com>
2025-03-11 12:55:33 +0100

    i2c builds now

--- sw/Maqueen/i2c_master.h
+++ sw/Maqueen/i2c_master.h
@@ -1,17 +1,10 @@
-/*
- * i2c_master.h
- *
- *  Created on: 4 mar. 2025
- *      Author: HP
- */
+// i2c_master.h
+#ifndef I2C_MASTER_H
+#define I2C_MASTER_H
 
-#ifndef I2C_MASTER_H_
-#define I2C_MASTER_H_
+#include <stdint.h>
 
-//******************************************************************************
-// General I2C State Machine ***************************************************
-//******************************************************************************
-
+// I2C Mode Enum
 typedef enum I2C_ModeEnum{
     IDLE_MODE,
     NACK_MODE,
@@ -20,44 +13,16 @@ typedef enum I2C_ModeEnum{
     TX_DATA_MODE,
     RX_DATA_MODE,
     SWITCH_TO_RX_MODE,
-    SWITHC_TO_TX_MODE,
+    SWITCH_TO_TX_MODE,
     TIMEOUT_MODE
 } I2C_Mode;
 
-/* I2C Write and Read Functions */
+// Function Prototypes
+void initGPIO(void);
+void initClockTo16MHz(void);
+void initI2C(uint8_t slave_addr);
+I2C_Mode I2C_Master_WriteReg(uint8_t dev_addr, uint8_t reg_addr, const uint8_t *reg_data, uint8_t count, I2C_Mode *MasterMode, uint8_t *TransmitRegAddr, uint8_t *TransmitBuffer, uint8_t *RXByteCtr, uint8_t *TXByteCtr, uint8_t *ReceiveIndex, uint8_t *TransmitIndex);
+I2C_Mode I2C_Master_ReadReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t count, I2C_Mode *MasterMode, uint8_t *TransmitRegAddr, uint8_t *RXByteCtr, uint8_t *TXByteCtr, uint8_t *ReceiveIndex, uint8_t *TransmitIndex);
+void CopyArray(const uint8_t *source, uint8_t *dest, uint8_t count);
 
-/* For slave device with dev_addr, writes the data specified in *reg_data
- *
- * dev_addr: The slave device address.
- *           Example: SLAVE_ADDR
- * reg_addr: The register or command to send to the slave.
- *           Example: CMD_TYPE_0_MASTER
- * *reg_data: The buffer to write
- *           Example: MasterType0
- * count: The length of *reg_data
- *           Example: TYPE_0_LENGTH
- *  */
-I2C_Mode I2C_Master_WriteReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t count);
-
-/* For slave device with dev_addr, read the data specified in slaves reg_addr.
- * The received data is available in ReceiveBuffer
- *
- * dev_addr: The slave device address.
- *           Example: SLAVE_ADDR
- * reg_addr: The register or command to send to the slave.
- *           Example: CMD_TYPE_0_SLAVE
- * count: The length of data to read
- *           Example: TYPE_0_LENGTH
- *  */
-I2C_Mode I2C_Master_ReadReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t count);
-void CopyArray(uint8_t *source, uint8_t *dest, uint8_t count);
-
-
-//******************************************************************************
-// Device Initialization *******************************************************
-//******************************************************************************
-void initGPIO_I2C();
-void initClockTo16MHz();
-void initI2C(uint8_t slave_addr)
-
-#endif /* I2C_MASTER_H_ */
+#endif // I2C_MASTER_H
--- sw/Maqueen/main.c
+++ sw/Maqueen/main.c
@@ -3,14 +3,107 @@
 
 #define SLAVE_ADDR  0x48
 
+// I2C EXAMPLE BEGINS
+//******************************************************************************
+// Pin Config ******************************************************************
+//******************************************************************************
+
+#define LED0_OUT    P1OUT
+#define LED0_DIR    P1DIR
+#define LED0_PIN    BIT0
+
+#define LED1_OUT    P6OUT
+#define LED1_DIR    P6DIR
+#define LED1_PIN    BIT6
+
+//******************************************************************************
+// Example Commands ************************************************************
+//******************************************************************************
+
+#define SLAVE_ADDR  0x48
+
+/* CMD_TYPE_X_SLAVE are example commands the master sends to the slave.
+ * The slave will send example SlaveTypeX buffers in response.
+ *
+ * CMD_TYPE_X_MASTER are example commands the master sends to the slave.
+ * The slave will initialize itself to receive MasterTypeX example buffers.
+ * */
+
+#define CMD_TYPE_0_SLAVE      0
+#define CMD_TYPE_1_SLAVE      1
+#define CMD_TYPE_2_SLAVE      2
+
+#define CMD_TYPE_0_MASTER      3
+#define CMD_TYPE_1_MASTER      4
+#define CMD_TYPE_2_MASTER      5
+
+#define TYPE_0_LENGTH   1
+#define TYPE_1_LENGTH   2
+#define TYPE_2_LENGTH   6
+
+#define MAX_BUFFER_SIZE     20
+
+/* MasterTypeX are example buffers initialized in the master, they will be
+ * sent by the master to the slave.
+ * SlaveTypeX are example buffers initialized in the slave, they will be
+ * sent by the slave to the master.
+ * */
+
+uint8_t MasterType2 [TYPE_2_LENGTH] = {'F', '4', '1', '9', '2', 'B'};
+uint8_t MasterType1 [TYPE_1_LENGTH] = { 8, 9};
+uint8_t MasterType0 [TYPE_0_LENGTH] = { 11};
+
+
+uint8_t SlaveType2 [TYPE_2_LENGTH] = {0};
+uint8_t SlaveType1 [TYPE_1_LENGTH] = {0};
+uint8_t SlaveType0 [TYPE_0_LENGTH] = {0};
+// I2C EXAMPLE ENDS
+
+// I2C UTILS
+/* Used to track the state of the software state machine*/
+I2C_Mode MasterMode = IDLE_MODE;
+
+/* The Register Address/Command to use*/
+uint8_t TransmitRegAddr = 0;
+
+/* ReceiveBuffer: Buffer used to receive data in the ISR
+ * RXByteCtr: Number of bytes left to receive
+ * ReceiveIndex: The index of the next byte to be received in ReceiveBuffer
+ * TransmitBuffer: Buffer used to transmit data in the ISR
+ * TXByteCtr: Number of bytes left to transfer
+ * TransmitIndex: The index of the next byte to be transmitted in TransmitBuffer
+ * */
+uint8_t ReceiveBuffer[MAX_BUFFER_SIZE] = {0};
+uint8_t RXByteCtr = 0;
+uint8_t ReceiveIndex = 0;
+uint8_t TransmitBuffer[MAX_BUFFER_SIZE] = {0};
+uint8_t TXByteCtr = 0;
+uint8_t TransmitIndex = 0;
+// I2C UTILS END
+
 /**
  * main.c
  */
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+    initClockTo16MHz();
+    initI2C(SLAVE_ADDR);
+
+    I2C_Master_WriteReg(SLAVE_ADDR, CMD_TYPE_0_MASTER, MasterType0, TYPE_0_LENGTH, &MasterMode, &TransmitRegAddr, &TransmitBuffer, &RXByteCtr, &TXByteCtr, &ReceiveIndex, &TransmitIndex);
+    I2C_Master_WriteReg(SLAVE_ADDR, CMD_TYPE_1_MASTER, MasterType1, TYPE_1_LENGTH, &MasterMode, &TransmitRegAddr, &TransmitBuffer, &RXByteCtr, &TXByteCtr, &ReceiveIndex, &TransmitIndex);
+    I2C_Master_WriteReg(SLAVE_ADDR, CMD_TYPE_2_MASTER, MasterType2, TYPE_2_LENGTH, &MasterMode, &TransmitRegAddr, &TransmitBuffer, &RXByteCtr, &TXByteCtr, &ReceiveIndex, &TransmitIndex);
+
+    I2C_Master_ReadReg(SLAVE_ADDR, CMD_TYPE_0_SLAVE, TYPE_0_LENGTH, &MasterMode, &TransmitRegAddr, &RXByteCtr, &TXByteCtr, &ReceiveIndex, &TransmitIndex);
+    CopyArray(ReceiveBuffer, SlaveType0, TYPE_0_LENGTH);
+
+    I2C_Master_ReadReg(SLAVE_ADDR, CMD_TYPE_1_SLAVE, TYPE_1_LENGTH, &MasterMode, &TransmitRegAddr, &RXByteCtr, &TXByteCtr, &ReceiveIndex, &TransmitIndex);
+    CopyArray(ReceiveBuffer, SlaveType1, TYPE_1_LENGTH);
+
+    I2C_Master_ReadReg(SLAVE_ADDR, CMD_TYPE_2_SLAVE, TYPE_2_LENGTH, &MasterMode, &TransmitRegAddr, &RXByteCtr, &TXByteCtr, &ReceiveIndex, &TransmitIndex);
+    CopyArray(ReceiveBuffer, SlaveType2, TYPE_2_LENGTH);
+
+    __bis_SR_register(LPM0_bits + GIE);
+    return 0;
 }
 
 //******************************************************************************
